35e41bc9a7aecb11895f87695fcc0142358ed2a0
Andrew Lee <alee14498@gmail.com>
2019-03-05 13:00:10 -0500

    Fixed a bunch of problems

--- system/boot.lua
+++ system/boot.lua
@@ -7,14 +7,14 @@ local desktop = "/system/desktop.lua"
 term.clear()
 term.setCursorPos(1,1)
 
-term.write("Starting up bits-UI ".. version .."...")
+print("Starting up bits-UI ".. version .."...")
 sleep(3)
 
 if term.isColor() then
     term.setTextColor(colors.green)
-    term.write("[OK] Advanced Computer is detected...")
+    print("[OK] Advanced Computer is detected...")
 else
-    term.write("[ERROR] You need a advanced computer in order to make the UI functional...")
+    print("[ERROR] You need a advanced computer in order to make the UI functional...")
     sleep(3)
     os.shutdown()
 end
@@ -23,10 +23,10 @@ sleep(3)
 
 if fs.exists(desktop) then
     term.setTextColor(colors.green)
-    term.write("[OK] Desktop has been found...")
+    print("[OK] Desktop has been found...")
 else
     term.setTextColor(colors.red)
-    term.write("[ERROR] Desktop cannot be found...")
+    print("[ERROR] Desktop cannot be found...")
     sleep(3)
     os.shutdown()
 end
@@ -35,29 +35,29 @@ sleep(3)
 
 if fs.exists("/home") then
     term.setTextColor(colors.green)
-    term.write("[OK] Home has been found...")
+    print("[OK] Home has been found...")
 else
     fs.makeDir("/home")
     term.setTextColor(colors.green)
-    term.write("[OK] Home directory has been created...")
+    print("[OK] Home directory has been created...")
 end
 
 sleep(3)
 
 if fs.exists("/system/bitUI.config") then
     term.setTextColor(colors.green)
-    term.write("[OK] Config has been found...")
+    print("[OK] Config has been found...")
 else
     config = io.open("/system/bitUI.config", "w")
     config:close()
     term.setTextColor(colors.blue)
-    term.write("[INFO] Config has not been found!")
-    term.write("[INFO] You will be sent to the OOBE setup...")
+    print("[INFO] Config has not been found!")
+    print("[INFO] You will be sent to the OOBE setup...")
 end
 
 sleep(3)
 term.setTextColor(colors.green)
-term.write("[DONE] Boot sequence is completed...")
+print("[DONE] Boot sequence is completed...")
 term.setTextColor(colors.white)
 sleep(3)
 shell.run(desktop)
